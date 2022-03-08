83ff4798f524b6d415eb693570dc4f339c32fd9e
redfly8 <redfly8@outlook.com>
2022-03-08 15:41:52 +0100

    start modul 2

---
+++ java/myFirstProject/src/lock.java
@@ -0,0 +1,75 @@
+//modul 2 aufgabe 1
+
+import java.util.Scanner;
+
+public class lock {
+
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*System.out.println("Bitte geben sie den 3 stelligen Pin ein!");
+        System.out.print("1. Ziffer: ");
+        a = scanner.nextInt();
+        System.out.print("2. Ziffer: ");
+        b = scanner.nextInt();
+        System.out.print("3. Ziffer: ");
+        c = scanner.nextInt();*/
+
+        /*
+          if (a == 0 && b == 0 && c == 70
+          ) {
+          System.out.println("offen");
+          } else {
+          System.out.println("falscher PIN.");
+         }
+         */
+        // obiges: Schritt 1
+
+       /* for (int i = 3; i >0; i--) {
+            System.out.println("Bitte geben sie den 3 stelligen Pin ein!");
+            System.out.print("1. Ziffer: ");
+            a = scanner.nextInt();
+            System.out.print("2. Ziffer: ");
+            b = scanner.nextInt();
+            System.out.print("3. Ziffer: ");
+            c = scanner.nextInt();
+
+            if (a == 0 && b == 0 && c == 7) {
+                System.out.println("offen");
+                break;
+            } else {
+                int z = i-1;
+                System.out.println("falscher PIN. Noch "+ z +" Versuch(e) übrig.");
+            }
+
+        }*/
+        //Schritt 2 (FOR SCHLEIFE)
+        /*boolean open = false;
+        while (open == false) {
+            System.out.println("Bitte geben sie den 3 stelligen Pin ein!");
+            System.out.print("1. Ziffer: ");
+            a = scanner.nextInt();
+            System.out.print("2. Ziffer: ");
+            b = scanner.nextInt();
+            System.out.print("3. Ziffer: ");
+            c = scanner.nextInt();
+
+            if (a == 0 && b == 0 && c == 7) {
+                System.out.println("offen");
+                open = true;
+            } else {
+                
+                System.out.println("falscher PIN.");
+            }
+            
+        }*/
+        boolean open = false;
+        byte versuch = 0;
+
+        while (open = false && versuch < 3) {
+            
+        }
+    }
+}
